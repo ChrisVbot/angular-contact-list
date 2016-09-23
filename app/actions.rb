4b7875a71406d30f0796f3f3c7c527af967f41f7
ChrisVbot <chris.veillette@gmail.com>
2016-09-23 02:02:17 +0000

    upping recent changes

--- app/actions.rb
+++ app/actions.rb
@@ -13,14 +13,22 @@ end
 #  # erb :index
 # end
 
-# post '/new/?' do
-#   # erb :index
-# end
+post '/api/new/' do
+  first_name = params[:firstname]
+  last_name = params[:lastname]
+  email = params[:email]
+  phone_number = params[:phoneno]
+  
+  @contact = Contact.new(first_name: first_name, last_name: last_name, email: email, phone_number:phone_number)
+  @contact.save
+  @contact.to_json
+end
 
-# get '/list/?' do
-#   # erb :index
-# end
+post '/api/delete/' do
+  info = params[:param]
+  @contact = Contact.where("email LIKE?", "%#{info}%")
+  @name = @contact[0]
+  @contact[0].destroy
+  @name.to_json
+end
 
-# post '/delete/?' do
-#   # erb :index
-# end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,28 +1,31 @@
 $(document).ready(function() {
-  // $('#search').on('click', function(){
-  //   $.getJSON()
-  // })
 
+
+  function getList(){
   $.ajax('/api/contacts/', {
     dataType: "json",
     success: function (data) {
       console.log(data);
-      var $contactList = $('#contactList');
+      var $contactList = $('.contactList');
+      $contactList.empty();
       populateList($contactList, data);
-      // for (var i = 0; i < data.length; i++) {
-      //   var contact = data[i];
-      //   var $newContact = $('<tr>');
-      //   var $contactName = $('<td>');
-      //   var $contactEmail = $('<td>');
-      //   var $contactPhone = $('<td>');
-      //   $contactName.text(contact.first_name + " " + contact.last_name);
-      //   $contactEmail.text(contact.email);
-      //   $contactPhone.text(contact.phone_number);
-      //   $newContact.append($contactName, $contactEmail, $contactPhone);
-      //   $contactList.append($newContact);
-      // }
     }
-  });
+    });
+  };
+
+    $('#list-all').on('click', function(event){
+      event.preventDefault();
+      var $listAll = $('#list-all');
+      var $contactList = $('.contactList');
+      $contactList.toggleClass('hidden show')
+      if ($contactList.hasClass('show')){
+        getList();
+        $listAll.text('Hide all contacts');
+      } else {
+        $listAll.text('Show all contacts');
+      };
+    });
+
 
   var populateList = function(list, contacts) {
       for (var i = 0; i < contacts.length; i++) {
@@ -36,27 +39,26 @@ $(document).ready(function() {
         $contactPhone.text(contact.phone_number);
         $newContact.append($contactName, $contactEmail, $contactPhone);
         list.append($newContact);
-      }
-  }
+      };
+  };
 
   var getPropertyValue = function(key) {
       return this[key];
-  }
+  };
 
   // The "this" value for the function below is the search term
   var valueThatInclude = function(value) {
     return value.toString().includes(this);
-  }
+  };
 
   // The "this" value for the function below is the search term. Returns true if the search term is contained within the object.
   var objectHasTermInValueFilter = function(someObj) {
     var allObjectValues = Object.keys(someObj).map(getPropertyValue, someObj);
     return allObjectValues.some(valueThatInclude, this);
-  }
+  };
 
-//attach submit handler to form
+
   $("#searchForm").submit(function(event){
-    //stop form from submitting normally
     event.preventDefault();
     var searchResults = $('#result');
     searchResults.empty();
@@ -71,6 +73,72 @@ $(document).ready(function() {
         // console.log('search results', data);
       }
     });
-  })
+  });
+
+
+  //Unhides the create new contact button on click
+  $("#create-new-contact").on('click', function(){
+    $(".new-contact").toggleClass('show hidden')
+  });
+
+
+  //Submits new contact via Ajax (formatted by json)
+  $("#new-contact").submit(function(event){
+    event.preventDefault();
+    var newContactCreate = $("#new-contact-create")
+    newContactCreate.empty();
+    var first_name = $('#first-name').val();
+    var last_name = $('#last-name').val();
+    var email = $('#email').val();
+    var phone_number = $('#phone').val();
+    var data = {firstname: first_name, lastname: last_name, email: email, phoneno: phone_number};
+    $.ajax({
+      method: "POST",
+      url:"/api/new/",
+      dataType: "json",
+      data: data, 
+    }).done(function(contact){
+      console.log("Successfully added new contact");
+      $("#new-contact")[0].reset();
+      populateList(newContactCreate, contact);
+      getList();
+      $(".new-contact").toggleClass('show hidden')
+    }).fail(function(jqXHR, textStatus){
+      console.log("Failed because: " + jqXHR.textStatus);
+    }).always(function(){
+      console.log("Always");
+    });
+  });
+
+
+  $('#delete-contact-button').on('click', function(){
+    $('.delete').toggleClass('show hidden');
+  });
+
+  $("#delete").submit(function(){
+    event.preventDefault();
+    var searchResults = $("#delete-result");
+    searchResults.empty();
+    var query = $("#d").val();
+    var data = {param: query};
+    var answer = confirm ("Are you sure you want to delete" + " " + query + " " + "from the database?");
+      if (answer){
+        $.ajax({
+          type: 'post',
+          dataType: "json",
+          url: "/api/delete/",
+          data: data,
+        }).done(function(contact){
+          console.log("Successful deletion");
+          console.log(contact);
+          getList();
+          $("#delete")[0].reset();
+        }).fail(function(jqXHR, textStatus){
+          console.log("Delete failed because: " + jqXHR.textStatus);
+        }).always(function(){
+          console.log("Always");
+        });
+      };
+    });
 
 });
